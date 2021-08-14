67e5850988c8b8d041eb155c63143f9a44e92670
Ishan Dongol <ishan.dongol@innovatetech.co>
2021-08-14 10:48:00 +0545

    Mongodb Integration

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,22 +1,28 @@
 use actix_web::{web,App, HttpServer};
-use slack_music_bot::{slack_events,index};
-// use std::env;
-// use mongodb::{options::ClientOptions, Client};
+use slack_music_bot::{slack_events,index,AppState};
+use dotenv::dotenv;
 
-// const DEFAULT_MONGO_URL:&str = "mongodb://localhost:21017/music";
+use std::env;
+use mongodb::{options::ClientOptions, Client};
+
+const DEFAULT_DATABASE_URL:&str = "mongodb://localhost:21017/music";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // let mongo_url:String = match env::var("MONGO_URL") {
-    //     Ok(val) => val,
-    //     Err(_) => String::from(DEFAULT_MONGO_URL)
-    // };
-    // let mut client_options = ClientOptions::parse(mongo_url).await.expect("Failed to connect to mongo");
-    // client_options.app_name = Some("Slack Bot".to_string());
-    // let client = Client::with_options(client_options).expect("No client");
-    // let db = client.database("music");
-    HttpServer::new(|| {
+    dotenv().ok();
+    let database_url:String = match env::var("DATABASE_URL") {
+        Ok(val) => val,
+        Err(_) => String::from(DEFAULT_DATABASE_URL)
+    };
+    let mut client_options = ClientOptions::parse(database_url).await.expect("Failed to connect to mongo");
+    client_options.app_name = Some("Slack Bot".to_string());
+    let client = Client::with_options(client_options).expect("No client");
+    let db = client.database("music");
+    HttpServer::new(move|| {
         App::new()
+            .data(AppState{
+                db:db.clone()
+            })
             .service(web::scope("/slack")
             .service(slack_events)
         )
