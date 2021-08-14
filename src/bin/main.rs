d9a9c0ef14d263d578a87f5bd7ef80e67fe8ea58
Ishan Dongol <ishan.dongol@innovatetech.co>
2021-08-14 10:27:08 +0545

    URl trim

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,26 +1,25 @@
-use actix_web::{App, HttpServer};
-use slack_music_bot::{slack_events,index,AppState};
-use std::env;
-use mongodb::{options::ClientOptions, Client};
+use actix_web::{web,App, HttpServer};
+use slack_music_bot::{slack_events,index};
+// use std::env;
+// use mongodb::{options::ClientOptions, Client};
 
-const DEFAULT_MONGO_URL:&str = "mongodb://localhost:21017/music";
+// const DEFAULT_MONGO_URL:&str = "mongodb://localhost:21017/music";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mongo_url:String = match env::var("MONGO_URL") {
-        Ok(val) => val,
-        Err(_) => String::from(DEFAULT_MONGO_URL)
-    };
-    let mut client_options = ClientOptions::parse(mongo_url).await.expect("Failed to connect to mongo");
-    client_options.app_name = Some("Slack Bot".to_string());
-    let client = Client::with_options(client_options).expect("No client");
-    let db = client.database("music");
-    HttpServer::new(move || {
+    // let mongo_url:String = match env::var("MONGO_URL") {
+    //     Ok(val) => val,
+    //     Err(_) => String::from(DEFAULT_MONGO_URL)
+    // };
+    // let mut client_options = ClientOptions::parse(mongo_url).await.expect("Failed to connect to mongo");
+    // client_options.app_name = Some("Slack Bot".to_string());
+    // let client = Client::with_options(client_options).expect("No client");
+    // let db = client.database("music");
+    HttpServer::new(|| {
         App::new()
-        .data(AppState {
-            db:db.clone()
-        })
+            .service(web::scope("/slack")
             .service(slack_events)
+        )
             .service(index)
     })
     .bind("0.0.0.0:5000")?
--- src/controllers/slack.rs
+++ src/controllers/slack.rs
@@ -2,11 +2,10 @@ use actix_web::{post,web, HttpResponse, Responder,HttpRequest};
 use serde::Deserialize;
 use regex::Regex;
 use lazy_static::lazy_static;
-use super::super::{AppState,Song};
+// use super::super::{AppState,Song};
 use mongodb::{bson::doc};
 
-#[derive(Deserialize)]
-#[derive(Debug)]
+#[derive(Debug,Deserialize)]
 struct SlackEvent{
   text:String,
   user:String,
@@ -14,18 +13,17 @@ struct SlackEvent{
   r#type: String,
 }
 
-#[derive(Deserialize)]
-#[derive(Debug)]
+#[derive(Debug,Deserialize)]
 struct SlackPayload {
   r#type: String,
   challenge: Option<String>,
   event: Option<SlackEvent>,
 }
 
-#[post("/slack/events")]
-async fn slack_events(_req: HttpRequest,body:web::Json<SlackPayload>,app_state: web::Data<AppState>) -> impl Responder {
+#[post("/events")]
+async fn slack_events(_req: HttpRequest,body:web::Json<SlackPayload>) -> impl Responder {
   if body.r#type == "url_verification" {
-    let value = body.challenge.as_ref().expect("No challenged");
+    let value = body.challenge.as_ref().expect("Not challenged");
     HttpResponse::Ok().body(value)
   } else if body.r#type == "event_callback" {
     let event = body.event.as_ref().expect("No event");
@@ -33,14 +31,14 @@ async fn slack_events(_req: HttpRequest,body:web::Json<SlackPayload>,app_state: 
       static ref RE:regex::Regex = Regex::new(r"https?://(www\.)?(youtube)+\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)").unwrap();
     }
     if event.r#type == "message" && RE.is_match(&event.text){
-      app_state.db.collection("playlist").insert_one(Song {
-        url: event.text.to_string(),
-        user: event.user.to_string(),
-        channel: event.channel.to_string(),
-        title: None,
-        description: None,
-      },None).await.expect("Failed to create");
-      println!("{}",&event.text);
+      // app_state.db.collection("playlist").insert_one(Song {
+      //   url: event.text.to_string(),
+      //   user: event.user.to_string(),
+      //   channel: event.channel.to_string(),
+      //   title: None,
+      //   description: None,
+      // },None).await.expect("Failed to create");
+      println!("{}",&event.text.trim_start_matches('<').trim_end_matches('>'));
     }
     if event.r#type == "app_mention" {
       println!("Mention");
