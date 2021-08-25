b3c0813ea68497cb183282686c23c593bca83652
Ishan Dongol <ishan.dongol@innovatetech.co>
2021-08-25 10:22:51 +0545

    Parse timestamp

--- src/services/rest.rs
+++ src/services/rest.rs
@@ -2,22 +2,48 @@ use actix_web::{get,web, HttpResponse, Responder,HttpRequest};
 use super::super::{AppState,Song};
 use mongodb::{bson::{doc},options::FindOptions}; 
 use futures::stream::{StreamExt};
-use chrono::Utc;
+use chrono::{Utc,DateTime,NaiveDateTime};
+use serde::{Deserialize, Serialize};
 
 #[get("/")]
 pub async fn index(_request: HttpRequest) -> impl Responder {
     HttpResponse::Ok().body("Welcome to slack_music_bot.rs")
 }
 
+#[derive(Debug, Serialize, Deserialize,Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct PlaylistQuery {
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub start_date: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub end_date: Option<String>
+}
 #[get("/playlist")]
-pub async fn playlist(_request: HttpRequest,app_state:web::Data<AppState>) -> impl Responder {
+pub async fn playlist(_request: HttpRequest,app_state:web::Data<AppState>,query:web::Query<PlaylistQuery>) -> impl Responder {
     let find_options = FindOptions::builder().projection(doc!{
         "channel":0,
         "user":0
     }).build();
     let mut cursor=  app_state.db.collection("playlist").find(doc!{
         "shared_on": {
-            "$gte": Utc::today().and_hms(0, 0, 0)
+            "$gte": match &query.start_date {
+                Some(start_date) => {
+                    let start_date = start_date.parse::<i64>().unwrap();
+                    DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(start_date, 0),Utc)
+                }
+                None => {
+                    Utc::today().and_hms(0, 0, 0)
+                }
+            },
+            "$lte":match &query.end_date {
+                Some(end_date) => {
+                    let end_date = end_date.parse::<i64>().unwrap();
+                    DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(end_date, 0),Utc)
+                }
+                None => {
+                    Utc::today().and_hms(23, 59, 59)
+                }
+            },
         }
     },find_options).await.expect("Failed mongo query");
     let mut playlist:Vec<Song> = Vec::new();
